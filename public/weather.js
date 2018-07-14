9d8151fc72c1ff110d23c57625671d8bc843a77b
unknown <Chriswalsh003@gmail.com>
2018-07-14 14:10:37 -0400

    cleanup

--- public/weather.js
+++ public/weather.js
@@ -1,7 +1,3 @@
-//import React from "react";
-//import ReactDOM from "react-dom";
-//import "./styles.css";
-
 class CardTitle extends React.Component {
   render() {
     return (
@@ -23,12 +19,13 @@ class Temperature extends React.Component {
 }
 
 class Container extends React.Component {
-
+  /*
   componentDidMount() {
     fetch('api.openweathermap.org/data/2.5/forecast?zip=30319,us&APPID=2ec71497636bbe75193d9958f68a8c5b&cnt=1&units=imperial')
       .then(response => response.json())
       .then(data => list = JSON.parse(data));
   }
+  */
 
   render() {
     return (
