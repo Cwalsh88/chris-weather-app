7151a11d65aa9afb436011ea4341b38c45a86e03
unknown <Chriswalsh003@gmail.com>
2018-07-14 12:10:35 -0400

    will find react?

--- public/weather.js
+++ public/weather.js
@@ -3,94 +3,48 @@ import ReactDOM from "react-dom";
 
 import "./styles.css";
 
-
-function Square(props) {
-  return (
-    <div class="box">
-      {props.day}
-      <div class="innerBox">
-        Temperature
-        <ul>
-          <li>12pm</li>
-          <li>1pm</li>
-          <li>2pm</li>
-          <li>3pm</li>
-          <li>4pm</li>
-        </ul>
+class CardTitle extends React.Component {
+  render() {
+    return (
+      <div className="cardTitle">
+        Card Title
       </div>
-    </div>
-  );
+    );
+  }
 }
 
-class APIData extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      error: null,
-      isLoaded: false,
-      items: []
-    };
+class Temperature extends React.Component {
+  render() {
+    return (
+      <div className="temperature"> 
+        Temperature
+      </div>
+      );
   }
+}
+
+class Container extends React.Component {
 
   componentDidMount() {
-    fetch("https://api.example.com/items")
-      .then(res => res.json())
-      .then(
-        (result) => {
-          this.setState({
-            isLoaded: true,
-            items: result.items
-          });
-        },
-        (error) => {
-          this.setState({
-            isLoaded: true,
-            error
-          });
-        }
-      )
+    fetch('api.openweathermap.org/data/2.5/forecast?zip=30319,us&APPID=2ec71497636bbe75193d9958f68a8c5b&cnt=1&units=imperial')
+      .then(response => response.json())
+      .then(data => list = JSON.parse(data));
   }
 
   render() {
-    const { error, isLoaded, items } = this.state;
-    if (error) {
-      return <div>Error: {error.message}</div>;
-    } else if (!isLoaded) {
-      return <div>Loading...</div>;
-    } else {
-      return (
-        <div>
-          {items}
-        </div>
-      );
-    }
-  }
-}
-
-class Main extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
-  renderSquare(i) {
-    return <Square day={i} />;
-  }
-
-  render() {
-    return(
-      <div>
-        <div className="boxContainer">
-          {this.renderSquare("Monday")}
-          {this.renderSquare("Tuesday")}
-          {this.renderSquare("Wednesday")}
-          {this.renderSquare("Thursday")}
-          {this.renderSquare("Friday")}
-        </div>
-        <APIData />
+    return (
+      <div className="box">
+        <CardTitle />
+        <Temperature />
       </div>
     );
   }
 }
 
 const rootElement = document.getElementById("root");
-ReactDOM.render(<Main />, rootElement);
+ReactDOM.render(<Container />, rootElement);
+
+var list = [];
+
+//Api will get passed into the Container down here.
+//Then passed down in the application as props.
