4b8160145737d3a70f0ce6602d962298ba7d69ea
unknown <Chriswalsh003@gmail.com>
2018-07-22 23:53:55 -0400

    Added dynamic days and current city

--- public/weather.js
+++ public/weather.js
@@ -49,11 +49,13 @@ class Card extends React.Component {
 
   render() {
     const temp = this.props.temp
-    const day = this.props.day
     const visible = this.props.visible
+    const date = new Date(temp.dt_txt)
+    const weekday = ["Sunday", "Monday", "Tuesday", "Wednesday",
+     "Thursday", "Friday", "Saturday"]
     return (
       <div className="card" style={{display: visible ? "inline-block" : "none"}}>
-        <span className="day">{day}</span>
+        <span className="day">{weekday[date.getDay()]}</span>
         <Temperature temp={temp} />
       </div>
     );
@@ -80,18 +82,18 @@ class Container extends React.Component {
   render() {
     console.log("Rendering")
     const temps = this.props.temps
+    const cityInfo = this.props.cityInfo
     const visibleDays = this.state.visibleDays
     return (
       <div className="app">
           <Title numOfDays={visibleDays} />
-          <Card day="Monday" temp={temps[0]} visible={visibleDays[0]} />
-          <Card day="Tuesday" temp={temps[8]} visible={visibleDays[1]}/>
-          <Card day="Wednesday" temp={temps[16]} visible={visibleDays[2]}/>
-          <Card day="Thursday" temp={temps[24]} visible={visibleDays[3]}/>
-          <Card day="Friday" temp={temps[32]} visible={visibleDays[4]}/>
-          <p>
-            <Button onClick={() => this.handleClick()} />
-          </p>
+          <Card temp={temps[0]} visible={visibleDays[0]} />
+          <Card temp={temps[8]} visible={visibleDays[1]}/>
+          <Card temp={temps[16]} visible={visibleDays[2]}/>
+          <Card temp={temps[24]} visible={visibleDays[3]}/>
+          <Card temp={temps[32]} visible={visibleDays[4]}/>
+          <p><Button onClick={() => this.handleClick()} /></p>
+          <p>Current City Displayed: {cityInfo.name}</p>
       </div>
     );
   }
@@ -99,31 +101,17 @@ class Container extends React.Component {
 
 console.log("Beginning of App, can write JS freely here");
 var apiTemps = [];
+var apiCity = [];
 fetch('https://api.openweathermap.org/data/2.5/forecast?zip=30319,us&APPID=2ec71497636bbe75193d9958f68a8c5b&cnt=40&units=imperial')
       .then(response => response.json())
       .then(data => {
       apiTemps = data.list; 
+      apiCity = data.city;
       const rootElement = document.getElementById("root");
-      ReactDOM.render(<Container temps={apiTemps}  />, rootElement);
+      ReactDOM.render(<Container temps={apiTemps} cityInfo={apiCity} />, rootElement);
     }); 
 
 //Change some components to functional components when done
-
-//tyle="color:blue;"
-
-/*
-
-    .filter((day,index) => index%8 == 0)
-
-            <ul>
-          {temps
-           .map((day,index) =>
-              <li key={day.dt}>
-                {day.main.temp}
-              </li>
-          )}
-        </ul>
-
-*/
+//Change the temps prop name. "temps" is actually a list of data for that day.
 
 
