44b32747abdd9dd0ccbc2d703254ec29a06ca18e
Christopher Walsh <Chriswalsh003@gmail.com>
2018-08-02 00:12:16 -0400

    API re-render + zipcode error checking

--- public/weather.js
+++ public/weather.js
@@ -1,65 +1,42 @@
-class Title extends React.Component {
-  constructor(props) {
-    super(props)
-  }
-
-  render() {
-    const num = this.props.numOfDays;
-    return (
-      <div className="title">
-        Check your {num.filter(x => x).length} day Forecast
-      </div>
-    )
-  }
+function Title(props) {
+  const num = props.numOfDays;
+  return (
+    <div className="title">
+      Check your {num.filter(x => x).length} day Forecast
+    </div>
+  )
 }
 
-class Button extends React.Component {
-  constructor(props) {
-    super(props)
-  }
-
-  render() {
-    return (
-      <button onClick={() => this.props.onClick()}>
-        Change the forecast between 3 or 5 days!
+function Button(props) {
+  const num = props.numOfDays;
+  return (
+      <button onClick={props.onClick}>
+        {num[4] ? "Collapse" : "Expand"} the Forecast
       </button>
-    )
-  }
+  )
 }
 
-class Temperature extends React.Component {
-  constructor(props) {
-    super(props)
-  }
-
-  render() {
-    const list = this.props.list
-    return (
-      <div className="temperature"> 
-        {list.main.temp}
-      </div>
-    )
-  }
+function Temperature(props) {
+  const list = props.list
+  return (
+    <div className="temperature"> 
+      {list.main.temp}
+    </div>
+  )
 }
 
-class Card extends React.Component {
-  constructor(props) {
-    super(props)
-  }
-
-  render() {
-    const {list, visible} = this.props
-    const date = new Date(list.dt_txt)
-    const weekday = ["Sunday", "Monday", "Tuesday", "Wednesday",
+function Card(props) {
+  const {list, visible} = props
+  const date = new Date(list.dt_txt)
+  const weekday = ["Sunday", "Monday", "Tuesday", "Wednesday",
      "Thursday", "Friday", "Saturday"]
-    return (
-      <div className="card" style={{display: visible ? "inline-block" : "none"}}>
-        <span className="day">{weekday[date.getDay()]}</span>
-        <Temperature list={list} />
-      </div>
-    )
-  }
-} 
+  return (
+    <div className="card" style={{display: visible ? "inline-block" : "none"}}>
+      <span className="day">{weekday[date.getDay()]}</span>
+      <Temperature list={list} />
+    </div>
+  )
+}
 
 class Form extends React.Component {
   constructor(props) {
@@ -86,8 +63,8 @@ class Form extends React.Component {
     return (
       <form onSubmit={this.submitInput}>
         <label>
-          Zipcode:
-            <input type="text" value={this.state.NewZipcode} onChange={this.updateState}/>
+          Enter a zipcode to change the city: 
+            <input type="text" pattern="[0-9]{5}" title="Must be 5 digits" value={this.state.NewZipcode} onChange={this.updateState}/>
         </label>
         <input type="submit" value="Submit" />
       </form>
@@ -99,8 +76,7 @@ class Container extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      list: [],
-      city: [],
+      weatherAPI: [],
       visibleDays: Array(5).fill(true),
       isLoading: true,
       zipcode: '30319'
@@ -114,7 +90,18 @@ class Container extends React.Component {
     this.setState({ isLoading: true})
     fetch('https://api.openweathermap.org/data/2.5/forecast?zip=30319,us&APPID=2ec71497636bbe75193d9958f68a8c5b&cnt=40&units=imperial')
       .then(response => response.json())
-      .then(data => this.setState({ list: data.list, city: data.city, isLoading: false}))
+      .then(data => this.setState({ weatherAPI: data, isLoading: false}))
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.zipcode !== this.state.zipcode) {
+      const zipcode = this.state.zipcode
+      const url = 'https://api.openweathermap.org/data/2.5/forecast?zip=' + zipcode + ',us&APPID=2ec71497636bbe75193d9958f68a8c5b&cnt=40&units=imperial'
+      this.setState({ isLoading: true})
+      fetch(url)
+        .then(response => response.json())
+        .then(data => this.setState({ weatherAPI: data, isLoading: false}))
+    }
   }
 
   handleClick() {
@@ -134,7 +121,7 @@ class Container extends React.Component {
 
   render() {
     console.log("Re-rendering")
-    const { list, city, visibleDays, isLoading, zipcode } = this.state
+    const { weatherAPI, visibleDays, isLoading, zipcode } = this.state
 
     if (isLoading) {
       console.log("Backing out of rendering until promise finishes")
@@ -144,13 +131,13 @@ class Container extends React.Component {
     return (
       <div className="app">
           <Title numOfDays={visibleDays} />
-          <Card list={list[0]} visible={visibleDays[0]} />
-          <Card list={list[7]} visible={visibleDays[1]} />
-          <Card list={list[15]} visible={visibleDays[2]} />
-          <Card list={list[23]} visible={visibleDays[3]} />
-          <Card list={list[31]} visible={visibleDays[4]} />
-          <p><Button onClick={() => this.handleClick()} /></p>
-          <p>Current City Displayed: {city.name}</p>
+          <Card list={weatherAPI.list[0]} visible={visibleDays[0]} />
+          <Card list={weatherAPI.list[7]} visible={visibleDays[1]} />
+          <Card list={weatherAPI.list[15]} visible={visibleDays[2]} />
+          <Card list={weatherAPI.list[23]} visible={visibleDays[3]} />
+          <Card list={weatherAPI.list[31]} visible={visibleDays[4]} />
+          <p><Button numOfDays={visibleDays} onClick={() => this.handleClick()} /></p>
+          <p>Current City Displayed: {weatherAPI.city.name}</p>
           <Form onSubmit={this.updateZipcode} />
       </div>
     )
@@ -162,8 +149,4 @@ console.log("Beginning of App")
 const rootElement = document.getElementById("root")
 ReactDOM.render(<Container />, rootElement)
 
-//Change some components to functional components when done
-
 //Get rid of the <p> tags in Card, Style the app properly through CSS
-
-//Expand and Collapse over Button
